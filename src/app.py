b96f4f66fc14e215beeb78b718670dc1f20b4701
crazz-zaac <r.alpha919@gmail.com>
2024-01-19 15:41:49 +0100

    removed radio box and added checkbox feature

--- src/app.py
+++ src/app.py
@@ -1,13 +1,39 @@
 import pandas as pd
 import streamlit as st
-
-from visualize import Visualization
+import math
+from popviz import PopVisualization
 
 from settings import DATASET_DIR
 from settings import SRC_DIR
 
 
-st.header("Visualizing best countries to live in 2024")
+st.header("Visualizing best countries :earth_africa: to live in 2024")
+
+def viewData(dataframe):
+    st.subheader("Dataset 2024")
+    items_per_page = 10
+    page_number = st.number_input("Select a page number", min_value=1, value=1)
+    
+    # Sort the DataFrame by the 'Company' column
+    total_pages = math.ceil(len(dataframe) / items_per_page)
+    page_number = max(1, min(page_number, total_pages))
+    
+    start_index = (page_number - 1) * items_per_page
+    end_index = min(start_index + items_per_page, len(dataframe))
+    
+    styled_df = dataframe.iloc[start_index:end_index].style
+            
+    # Display dataframe with pagination
+    styled_df = styled_df.set_table_styles(
+        [{'selector': 'table', 
+            'props': [('width', '800px'), ('height', '800px')]
+            }]
+    )
+    st.write(styled_df)
+    
+    # Display "Page X of Y" indicator
+    st.text(f"Page {page_number} of {total_pages}")
+
 
 def main():
     raw_data = pd.read_csv(DATASET_DIR+'/'+'best-countries-2024.csv')
@@ -15,15 +41,28 @@ def main():
     # data cleaning
     clean_df = raw_data.dropna()
     
-    viz = Visualization(clean_df)
     
-    st.sidebar.subheader("Select an option")
-    choice = st.sidebar.radio("Choose one", 
-                     ('View data', 'Visualize data'))
+    viz = PopVisualization(clean_df)
     
-    if choice == 'View data':
-        viz.viewData()
+    st.sidebar.subheader("Select one or more option")
     
+    view_dataset = st.sidebar.checkbox('View dataset')
+    pop_growth_rate = st.sidebar.checkbox('Population growth rate')
+    region_wise_pop = st.sidebar.checkbox('Region wise population')
+    region_pop_dist = st.sidebar.checkbox('Region wise population distribution')
+    
+    
+    if view_dataset:
+        viewData(clean_df)
+    
+    if pop_growth_rate:
+        viz.popVsPopGrowth()
+    
+    if region_wise_pop:
+        viz.regionPop()
+        
+    if region_pop_dist:
+        viz.regionMemPop()
 
 
 if __name__ == '__main__':
